2dfc32b5fd7c795e30b4d69d36bcf1e531b64ad2
Matheus <matditomate@gmail.com>
2025-10-26 21:15:08 -0300

    Descomentando código commitado

--- demo/src/test/java/ValidadorSenhaTest.java
+++ demo/src/test/java/ValidadorSenhaTest.java
@@ -10,9 +10,9 @@ public class ValidadorSenhaTest {
     public void testValidarSenha() {
         ValidadorSenha val = new ValidadorSenha();
         assertTrue(val.validar("Senha123"));
-        // assertFalse(val.validar("senha123")); // falta maiúscula
-        // assertFalse(val.validar("SENHA"));    // falta número
-        // assertFalse(val.validar("123456"));   // falta letra
-        // assertFalse(val.validar(""));         // vazia
+        assertFalse(val.validar("senha123")); // falta maiúscula
+        assertFalse(val.validar("SENHA"));    // falta número
+        assertFalse(val.validar("123456"));   // falta letra
+        assertFalse(val.validar(""));         // vazia
     }
 }
